567f4470d54a2154517148a9ee8e16b17a38df2c
HumonFromWalnart <tlmuunulzii@gmail.com>
2022-12-29 19:47:15 +0800

    Frickin comment mistake

--- frontend/src/userProfile.js
+++ frontend/src/userProfile.js
@@ -40,6 +40,37 @@ export const UserProfile = () => {
                     );
                 })
             }
+            {/* {
+                data.map((cur) => {
+                    return (
+                        <div className="BigContainer">
+                            <div className="inventory">
+                                <img src={cur.weapons?.primary} id="weaponMain"></img>
+                                {/* <p>Shotgun</p> */}
+                                {/* <div className="secondHand"> */}
+                                    {/* <div id="Secondary"> */}
+                                        {/* r        <img src={cur.weapons?.secondary} id="weapon"></img> */}
+                                        {/* <p>Pistol</p> */}
+                                    {/* </div> */}
+                                    {/* <div id="Melee"> */}
+                                        {/* <img src={cur.weapons?.melee} id="weapon"></img> */}
+                                        {/* <p>Wrench</p> */}
+                                    {/* </div>
+                                </div>
+                            </div>
+                            <div className="tape">
+                                <a href={cur.email} id='link'>More about me</a>
+                            </div>
+                            <div className="fullName">
+                                <div id="firstName">{cur.firstName}</div>
+                                <div id="lastName">{cur.lastName}</div>
+                            </div>
+                            <img src={cur.picture} id="picture"></img>
+                            <div>{cur.line}</div>
+                        </div> */}
+                    {/* ); */}
+                {/* }) */}
+            {/* } */} 
         </div>
     );
 }
